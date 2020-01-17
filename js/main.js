248290e7906d72616d5ee14e0a1146058dbd9710
DrIssah <52053029+DrIssah@users.noreply.github.com>
2020-01-17 16:43:17 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+jQuery.noConflict()(function ($) { 
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+   
+    animate();
+    
+    
+function animate() {
+  var elem = document.getElementById("para");   
+  var pos = 0;
+  var opac = 1;
+  var id = setInterval(frame, 50);
+  var paraWidth = $( "#para" ).width();
+  var holderWidth = $( "#holder" ).width();
+    
+  console.log( "Parawidth" + paraWidth);
+  function frame() {
+    if (pos == 400) {
+      clearInterval(id);
+      animate();
+    } else {
+      pos = pos + 1; 
+     
+      elem.style.marginLeft  = pos + "px";
+      elem.style.opacity  = opac;
+    
+      
+    }
+  }
+}
+    
+    
+    
+    
+});
+    
+});
+
+
